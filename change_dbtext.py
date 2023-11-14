f244289a498a58f995904ad4bc2a55d3e272704b
Masaki-Okuyama <masaki.zinbei@gmail.com>
2023-11-14 23:32:40 +0900

    画像URLを正常なものに

--- change_dbtext.py
+++ change_dbtext.py
@@ -16,12 +16,12 @@ def main():
             card = line.split(" | ")
             card_list.append(card)
 
-    # with open('noimage.txt', 'r', encoding='utf-8') as file:
-    #     # ファイル内の各行を順に読み込む
-    #     for line in file:
-    #         line = line.strip() # 行の先頭と末尾の余分な空白文字を削除
-    #         card = line.split(" | ")
-    #         noimage_list.append(card)
+    with open('no_image.txt', 'r', encoding='utf-8') as file:
+        # ファイル内の各行を順に読み込む
+        for line in file:
+            line = line.strip() # 行の先頭と末尾の余分な空白文字を削除
+            card = line.split(" | ")
+            noimage_list.append(card)
 
     # cidが同じものを探す
     # returnは2次元配列
@@ -39,18 +39,18 @@ def main():
     #     if not card_data[4] == '-':
     #         card_data[7] = get_correct_cardtype(card_data[14])
 
-    for card_data in card_list:
-        card_data[7] = card_data[7].replace("'", "\"")
+    # for card_data in card_list:
+    #     card_data[7] = card_data[7].replace("'", "\"")
 
     # no-imageを見つける
     # returnは2次元配列
-    # matching_rows = get_rows_with_element(card_list, 'https://ocg-card.com/img/card/ocg/dama-062.jpg')
+    # matching_rows = get_rows_with_element(card_list, 'image-not-found')
 
     # noimageとcardlist比べて同じデータあったら差し替える
-    # for data_row in card_list:
-    #     for no_row in noimage_list:
-    #         if data_row[0] == no_row[0]:
-    #             data_row[16] = no_row[16]
+    for data_row in card_list:
+        for no_row in noimage_list:
+            if data_row[0] == no_row[0]:
+                data_row[16] = no_row[16]
 
     with open('buff.txt', 'w', encoding='utf-8') as file:
         # テキストをファイルに書き込む
